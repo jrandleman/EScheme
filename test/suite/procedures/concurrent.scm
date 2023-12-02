01e4cf4cde034cb08b63f081987575c79d1cfc31
Jordan Randleman <jordanran199@gmail.com>
2023-12-02 01:27:35 -0800

    Update concurrent.scm

--- test/suite/procedures/concurrent.scm
+++ test/suite/procedures/concurrent.scm
@@ -273,7 +273,6 @@
 (ut (await 10 (promise (lambda (resolve reject) (sleep 200) 100)) add1 id) 'timeout)
 
 (define (promise-1) (promise (lambda (a b) 1)))
-
 (ut (await (await-all (promise-1) (promise-1)) (+ map add1) (+ map add2)) '(2 2))
 (ut (await (await-all (list (promise-1) (promise-1))) (+ map add1) (+ map add2)) '(2 2))
 (ut (await (await-all [(promise-1) (promise-1)]) (+ map add1) (+ map add2)) '(2 2))
