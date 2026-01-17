05a30d4648166b2d6cd51c4a30b3fbe2dd6ba65a
Jordan Randleman <jordanran199@gmail.com>
2026-01-17 13:19:24 -0800

    Improves help message for object instances

--- src/escm/type/oo/EscmObject.java
+++ src/escm/type/oo/EscmObject.java
@@ -11,6 +11,7 @@
 //    - callWith(...) // overloads application via a "->procedure" method!
 
 package escm.type.oo;
+
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 import escm.util.Trampoline;
@@ -29,7 +30,6 @@ public class EscmObject extends MetaObject implements Callable {
     return escmClass;
   }
 
-
   ////////////////////////////////////////////////////////////////////////////
   // Interfaces Implemented
   public boolean hasInterface(EscmInterface iface) {
@@ -40,7 +40,6 @@ public class EscmObject extends MetaObject implements Callable {
     escmClass.forEachInterface(ip);
   }
 
-
   ////////////////////////////////////////////////////////////////////////////
   // Super Object
   private EscmObject superObject = null;
@@ -49,48 +48,56 @@ public class EscmObject extends MetaObject implements Callable {
     return superObject;
   }
 
-
   ////////////////////////////////////////////////////////////////////////////
   // Internal Constructor
   // @PRECONDITION: <superObject> MUST ALREADY BE INITIALIZED!
-  EscmObject(EscmClass escmClass, EscmObject superObject, ConcurrentHashMap<String,Datum> props) throws Exception {
+  EscmObject(EscmClass escmClass, EscmObject superObject, ConcurrentHashMap<String, Datum> props) throws Exception {
     this.escmClass = escmClass;
     this.superObject = superObject;
     this.props = props;
-    this.props.put("class",this.escmClass);
+    this.props.put("class", this.escmClass);
     this.convertProceduresToMethods();
   }
-
 
   ////////////////////////////////////////////////////////////////////////////
   // Documentation String
   public String docstring() {
-    return escmClass.docstring();
+    StringBuilder sb = new StringBuilder("Object instance of class: ");
+    sb.append(escmClass.readableName());
+    int n = sb.length();
+    sb.append('\n');
+    for (int i = 0; i < n; i++)
+      sb.append('-');
+    sb.append("\n\n");
+    sb.append(escmClass.docstring());
+    return sb.toString();
   }
-
 
   ////////////////////////////////////////////////////////////////////////////
   // Functor Logic (Application Overload)
   public Datum signature() {
     try {
-      if(!has("->procedure")) return Boolean.FALSE;
+      if (!has("->procedure"))
+        return Boolean.FALSE;
       Datum procedure = get("->procedure");
-      if(!(procedure instanceof MethodProcedure)) return Boolean.FALSE;
-      return EscmClass.tagMethodWithName(this,((MethodProcedure)procedure).signature());
-    } catch(Exception e) {
+      if (!(procedure instanceof MethodProcedure))
+        return Boolean.FALSE;
+      return EscmClass.tagMethodWithName(this, ((MethodProcedure) procedure).signature());
+    } catch (Exception e) {
       return Boolean.FALSE;
     }
   }
 
   public Trampoline.Bounce callWith(ArrayList<Datum> args, Trampoline.Continuation continuation) throws Exception {
-    if(!has("->procedure"))
-      throw new Exceptionf("Object of class %s error: can't be applied without a \"->procedure\" method!", escmClass.readableName());
+    if (!has("->procedure"))
+      throw new Exceptionf("Object of class %s error: can't be applied without a \"->procedure\" method!",
+          escmClass.readableName());
     Datum procedure = get("->procedure");
-    if(!(procedure instanceof MethodProcedure))
-      throw new Exceptionf("Object of class %s error: \"->procedure\" property %s isn't a method!", escmClass.readableName(),procedure.profile());
-    return ((MethodProcedure)procedure).callWith(args,continuation);
+    if (!(procedure instanceof MethodProcedure))
+      throw new Exceptionf("Object of class %s error: \"->procedure\" property %s isn't a method!",
+          escmClass.readableName(), procedure.profile());
+    return ((MethodProcedure) procedure).callWith(args, continuation);
   }
-
 
   ////////////////////////////////////////////////////////////////////////////
   // Immediate Method Name Generation
@@ -103,42 +110,41 @@ public class EscmObject extends MetaObject implements Callable {
     return sb.toString();
   }
 
-
   ////////////////////////////////////////////////////////////////////////////
   // Functor Predicate
   public boolean isFunctor() {
     return has("->procedure");
   }
 
-
   ////////////////////////////////////////////////////////////////////////////
-  // Update the Super Object (& return the updated super) (equivalent to the "super!" macro)
+  // Update the Super Object (& return the updated super) (equivalent to the
+  //////////////////////////////////////////////////////////////////////////// "super!"
+  //////////////////////////////////////////////////////////////////////////// macro)
   protected void bindMethodsWithNewSelfAndSuper() {
-    for(ConcurrentHashMap.Entry<String,Datum> e : props.entrySet()) {
+    for (ConcurrentHashMap.Entry<String, Datum> e : props.entrySet()) {
       Datum val = e.getValue();
-      if(val instanceof MethodProcedure) {
-        props.put(e.getKey(),((MethodProcedure)val).loadWithNewSelfAndSuper(this,this.superObject));
+      if (val instanceof MethodProcedure) {
+        props.put(e.getKey(), ((MethodProcedure) val).loadWithNewSelfAndSuper(this, this.superObject));
       }
     }
   }
 
-
-  public Trampoline.Bounce updateSuper(ArrayList<Datum> parameters, Trampoline.Continuation continuation) throws Exception {
-    if(superObject == null)
+  public Trampoline.Bounce updateSuper(ArrayList<Datum> parameters, Trampoline.Continuation continuation)
+      throws Exception {
+    if (superObject == null)
       throw new Exceptionf("'super! class %s doesn't have a <super> to initialize!", escmClass.readableName());
-    return superObject.escmClass.callWith(parameters,(newSuperObj) -> () -> {
+    return superObject.escmClass.callWith(parameters, (newSuperObj) -> () -> {
 
       ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-      // @NOTE: This is ONLY thread safe if the user ONLY uses <super!> as the 
-      //        1st statement in the constructor (as directed by the README)
+      // @NOTE: This is ONLY thread safe if the user ONLY uses <super!> as the
+      // 1st statement in the constructor (as directed by the README)
       ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-      
-      this.superObject = (EscmObject)newSuperObj;
+
+      this.superObject = (EscmObject) newSuperObj;
       this.bindMethodsWithNewSelfAndSuper();
       return continuation.run(this.superObject);
     });
   }
-
 
   ////////////////////////////////////////////////////////////////////////////
   // Instance of Interface or Class
@@ -146,42 +152,41 @@ public class EscmObject extends MetaObject implements Callable {
     public boolean value = false;
   }
 
-
   private boolean interfaceImplementsInterface(EscmInterface i1, EscmInterface i2) {
     BooleanBox bb = new BooleanBox();
     i1.forEachInterface((iface) -> {
-      bb.value = iface.eq(i2) || interfaceImplementsInterface(iface,i2);
+      bb.value = iface.eq(i2) || interfaceImplementsInterface(iface, i2);
       return !bb.value;
     });
     return bb.value;
   }
 
-
   public boolean instanceOf(EscmInterface i) {
     EscmObject obj = this;
-    while(obj != null) {
-      if(obj.escmClass.hasInterface(i)) return true;
+    while (obj != null) {
+      if (obj.escmClass.hasInterface(i))
+        return true;
       BooleanBox bb = new BooleanBox();
       obj.escmClass.forEachInterface((iface) -> {
-        bb.value = interfaceImplementsInterface(iface,i);
+        bb.value = interfaceImplementsInterface(iface, i);
         return !bb.value;
       });
-      if(bb.value) return true;
+      if (bb.value)
+        return true;
       obj = obj.superObject;
     }
     return false;
   }
-
 
   public boolean instanceOf(EscmClass c) {
     EscmObject obj = this;
-    while(obj != null) {
-      if(obj.escmClass == c) return true;
+    while (obj != null) {
+      if (obj.escmClass == c)
+        return true;
       obj = obj.superObject;
     }
     return false;
   }
-
 
   ////////////////////////////////////////////////////////////////////////////
   // Type
@@ -189,28 +194,29 @@ public class EscmObject extends MetaObject implements Callable {
     return "object";
   }
 
-
   ////////////////////////////////////////////////////////////////////////////
   // Equality
   public boolean eq(Object o) {
-    return o instanceof EscmObject && ((EscmObject)o).props == this.props;
+    return o instanceof EscmObject && ((EscmObject) o).props == this.props;
   }
 
   public boolean equal(Object o) {
-    if(!(o instanceof EscmObject) || ((EscmObject)o).escmClass != escmClass) return false;
-    EscmObject that = (EscmObject)o;
+    if (!(o instanceof EscmObject) || ((EscmObject) o).escmClass != escmClass)
+      return false;
+    EscmObject that = (EscmObject) o;
     // Check Equality of Props
-    if(this.props.size() != that.props.size()) return false;
-    for(ConcurrentHashMap.Entry<String,Datum> e : this.props.entrySet()) {
+    if (this.props.size() != that.props.size())
+      return false;
+    for (ConcurrentHashMap.Entry<String, Datum> e : this.props.entrySet()) {
       Datum thisProp = e.getValue();
       Datum thatProp = that.props.get(e.getKey());
-      if(thatProp == null) {
+      if (thatProp == null) {
         return false;
       }
-      if(thisProp instanceof MethodProcedure && thatProp instanceof MethodProcedure) {
-        if(!((MethodProcedure)thisProp).name().equals(((MethodProcedure)thatProp).name()))
+      if (thisProp instanceof MethodProcedure && thatProp instanceof MethodProcedure) {
+        if (!((MethodProcedure) thisProp).name().equals(((MethodProcedure) thatProp).name()))
           return false;
-      } else if(!thisProp.equal(thatProp)) {
+      } else if (!thisProp.equal(thatProp)) {
         return false;
       }
     }
@@ -218,12 +224,12 @@ public class EscmObject extends MetaObject implements Callable {
     return this.superObject == null || this.superObject.equal(that.superObject);
   }
 
-
   ////////////////////////////////////////////////////////////////////////////
   // Serialization
   public String display() {
     String className = escmClass.name();
-    if(className.length() == 0) return "#<object>";
+    if (className.length() == 0)
+      return "#<object>";
     return "#<object (class=" + className + ")>";
   }
 
@@ -235,20 +241,19 @@ public class EscmObject extends MetaObject implements Callable {
     return write();
   }
 
-
   ////////////////////////////////////////////////////////////////////////////
   // Loading-into-environment semantics for the VM's interpreter
   public EscmObject loadWithName(String name) {
     return this;
   }
 
-
   ////////////////////////////////////////////////////////////////////////////
   // Copying (performs a shallow copy up the inheritance chain)
   // <ignore> is used to distinguish this ctor from the above public one
-  private EscmObject(int ignore, EscmClass escmClass, EscmObject superObject, ConcurrentHashMap<String,Datum> propsCopy) {
+  private EscmObject(int ignore, EscmClass escmClass, EscmObject superObject,
+      ConcurrentHashMap<String, Datum> propsCopy) {
     this.escmClass = escmClass;
-    if(superObject == null) {
+    if (superObject == null) {
       this.superObject = null;
     } else {
       this.superObject = superObject.shallowCopy();
@@ -258,6 +263,6 @@ public class EscmObject extends MetaObject implements Callable {
   }
 
   public EscmObject shallowCopy() {
-    return new EscmObject(0,escmClass,superObject,new ConcurrentHashMap<String,Datum>(props));
+    return new EscmObject(0, escmClass, superObject, new ConcurrentHashMap<String, Datum>(props));
   }
 }
