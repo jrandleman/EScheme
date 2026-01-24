c1520a20aab1fa0c9ef3a17ef30a8d0bc0387f01
Jordan Randleman <jordanran199@gmail.com>
2026-01-24 01:41:09 -0800

    added emojis to unit tests lol

--- test/main.scm
+++ test/main.scm
@@ -18,7 +18,8 @@
   (verbose-displayf "    %n) Executing Test File: %wa ..." total-files test-file-name)
   (define time-length test-result (time system (append *escm-execution-command* "\"" file "\"")))
   (define output errput errcode test-result)
-  (verbose-displayf " Time: %nms; Exit Code: %n\n" time-length errcode)
+  (define result-emoji (if (zero? errcode) "\u2705" "\ud83d\udea8"))
+  (verbose-displayf " Time: %nms; Exit Code: %n %s\n" time-length errcode result-emoji)
   (if (not (= 0 errcode))
       (set! total-errors (+ total-errors 1)))
   (if (not (empty? errput))
@@ -36,15 +37,17 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; MAIN EXECUTION
 (define (tests-main-execution)
-  (verbose-displayf ">>> Processing Test Files:\n")
+  (verbose-displayf ">>> Processing Test Files (please be patient \ud83d\ude05):\n")
   (define start (epoch-time))
   (test-all-files (path #path "suite"))
   (define end (epoch-time))
-  (verbose-displayf ">>> Total Files: %n; Total Errors: %n; Success Rate: %.2n%%; Total Time: %.2ns\n" 
+  (define result-emoji (if (zero? total-errors) "\u2705" "\ud83d\udea8"))
+  (verbose-displayf ">>> Total Files: %n; Total Errors: %n; Success Rate: %.2n%%; Total Time: %.2ns %s\n" 
     total-files 
     total-errors 
     (* 100.0 (/ (- total-files total-errors) total-files))
-    (/ (- end start) 1000.0))
+    (/ (- end start) 1000.0)
+    result-emoji)
   (exit total-errors))
 
 (tests-main-execution)
